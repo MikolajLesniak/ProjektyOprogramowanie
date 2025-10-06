9ec81cc43cc9ec306b13482df49ef621e2bbdf84
MikolajLesniak <260816@edu.p.lodz.pl>
2025-10-06 11:18:37 +0200

    02 Zmiana metody Policz

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 int Policz(int a, int b) {
-    return a+b;
+    return a*b;
 }
 
 int main() {
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
-    std::cout<<"W pierwszym branchu; 2+3="<<Policz(2,3)<<std::endl;
+    std::cout<<"W pierwszym branchu; 2*3="<<Policz(2,3)<<std::endl;
 
 
     for (int i = 1; i <= 5; i++) {
